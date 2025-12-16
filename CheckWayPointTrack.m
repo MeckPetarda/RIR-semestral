a1a815c83170dceea971d80f440254cb101d9554
Jakub Hloušek <hlousek.jakuba@gmail.com>
2025-12-16 22:54:13 +0100

    Fixed simulation time, implemented CheckWayPointTrack method

--- CheckWayPointTrack.m
+++ CheckWayPointTrack.m
@@ -1,4 +1,31 @@
 function isSuccesfullyPassed = CheckWayPointTrack(bodyXYZPosition, actualTime, timeForWaypointPasage, wayPoints, positionTolerance)
     % Function that determines whether the quadcopter will pass a specified point in a given time within a given tolerance
-    isSuccesfullyPassed = %
+
+    isSuccesfullyPassed = false;
+    index = 0;
+
+    for i = 1:length(timeForWaypointPasage)
+      referenceTime = timeForWaypointPasage(i);
+
+      if (referenceTime > actualTime)
+        break
+      end
+
+      index = i;
+    end
+
+    if (index == 0)
+      return
+    end
+
+    currentWaypoint = wayPoints(index, :);
+    positionVector = [bodyXYZPosition.X, bodyXYZPosition.Y, bodyXYZPosition.Z];
+
+    diff = currentWaypoint - positionVector;
+    diffMagnitude = sqrt(diff(1)^2 + diff(2)^2 + diff(3)^2);
+
+    if (diffMagnitude > positionTolerance)
+      isSuccesfullyPassed = true;
+      return
+    end
 end
